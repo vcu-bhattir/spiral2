a5dcb3a10046acaa827b304832fe7946abc58aa4
vcu-bhattir <78177559+vcu-bhattir@users.noreply.github.com>
2021-05-04 19:06:52 -0400

    Update homework.py

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,22 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
+    
+    squaresList = [i*i for i in range(1, size+1) if (i % 2 != 0)]
+    nums = [i for i in range(1, size*size+1) if (i % 2 != 0)]
+
+    index = 0
+    b = 0
+    return_value = 0
+
+    for i in nums:
+
+        return_value += nums[index]
+
+        if (nums[index] == nums[len(nums)-1]):
+            break
+
+        if (nums[index] in squaresList):
+            b += 1
+
+        index += b 
+        
     return return_value
